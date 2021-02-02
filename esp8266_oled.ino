005ee4a54035fe7539724409ac95640ba13e2d2d
AaronChuzb <45120061+AaronChuzb@users.noreply.github.com>
2021-02-02 14:05:33 +0800

    *add 页面框架的一些更改

--- esp8266_oled.ino
+++ esp8266_oled.ino
@@ -1,4 +1,7 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <FS.h> //闪存文件系统
+#include <ArduinoJson.h>//json数据处理库（第三方）
 #include <Wire.h>
 #include "time.h"
 #include <Ticker.h>
@@ -10,8 +13,11 @@
 #include "weathericons.h"
 #include "selectweatherfont.h"
 
+
+
 /* ----------------------------------------------------------全局数据定义以及特殊变量声明--------------------------------------------------------------- */
 
+ESP8266WebServer server(80); //创建Web服务端口为80
 SSD1306Wire  display(0x3c, 2, 0);
 OLEDDisplayUi ui     ( &display );
 
@@ -55,43 +61,6 @@ int KEY_NUM           = 0; //按键编号
 
 const int DISPLAY_LIGHT_MODE = 1; //显示模式 1：长亮，0：30秒后自动息屏
 String IP_ADDRESS;                //IP地址
-
-
-
-/* ----------------------------------------------------------在没进入框架前的处理--------------------------------------------------------------- */
-
-/* 单个进度条方法 */
-void drawProgress(OLEDDisplay *display, int progress, String label) {
-  display->clear();
-  display->setTextAlignment(TEXT_ALIGN_CENTER);
-  display->setFont(ArialMT_Plain_10);
-  display->drawString(64, 10, label);
-  display->drawProgressBar(2, 28, 124, 10, progress);
-  display->display();
-}
-/* 初次更新进度 */
-void upDateProgress(OLEDDisplay *display) {  //首次天气更新
-  drawProgress(display, 0, "");
-  wificonnect();
-  drawProgress(display, 33, "Please wait Updating weather...");
-  configTime(TZ_SEC, DST_SEC, "ntp.ntsc.ac.cn", "ntp1.aliyun.com"); //ntp获取时间同步系统时间戳
-  drawProgress(display, 66, "Please wait Updating forecasts...");
-  weatherNow.config(UserKey, Location, Unit, Lang);
-  if(weatherNow.get()){ // 获取天气更新
-    int icon_code = weatherNow.getIcon();
-    WEATHER_ICON = getIcons(String(icon_code));
-    TEMP =  String(weatherNow.getTemp());
-    String type = getWeatherType(icon_code);
-    HUM = weatherNow.getHumidity();
-    WIND = weatherNow.getWindScale();
-    length = type.length() / 3;
-    for(int i =0 ; i<type.length(); i++ ) {
-      WEATHER_TYPE[i] = type[i];
-    }
-  }
-  drawProgress(display, 100, "Wellcome...");
-  delay(200);
-}
 
 
 /* ----------------------------------------------------------框架页面函数--------------------------------------------------------------- */
@@ -140,21 +109,20 @@ void drawWeather(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int
   display->drawString(x + 0, y+0  , WEATHER_ICON);
   display->setTextAlignment(TEXT_ALIGN_LEFT);
   display->setFont(DSEG7_Classic_Regular_32);
-  display->drawString(x + 32, y + 2, TEMP);
+  display->drawString(x + 35, y + 2, TEMP);
   // display->setFont(Cousine_Regular_36);
   // display->drawString(x + 72, y + 2, "°C");
-  display->drawXbm(x + 85, y + 2, 40, 40, c);
+  display->drawXbm(x + 88, y + 2, 40, 40, c);
   int pos;
-  if(length == 1) { pos = 16; }
+  if(length == 1) { pos = 12; }
   if(length == 2) { pos = 10; }
   if(length == 3) { pos = 4; }
   if(length >= 4) { pos = 0; }
   drawChinese(display, pos + x, 38 + y, WEATHER_TYPE);
   drawChinese(display, 60 + x, 38 + y, "湿度");
   display->setFont(Dialog_plain_12);
-  display->drawString(x + 100, y + 38, ':' + String(int(HUM)) + '%');
+  display->drawString(x + 88, y + 38, ':' + String(int(HUM)) + '%');
 }
-
 
 void drawFrame3(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int16_t y) {
   display->setFont(ArialMT_Plain_10);
@@ -176,15 +144,91 @@ void drawFrame5(OLEDDisplay *display, OLEDDisplayUiState* state, int16_t x, int1
   display->drawXbm(96+x, 5+y, 16,16, ri);
 }
 
-/* 页面框架定义 */
-FrameCallback frames[] = { drawDateTime, drawWeather, drawFrame3, drawFrame4, drawFrame5 };
-int frameCount = 5;
 
-OverlayCallback overlays[] = { drawIpOverlay };
-int overlaysCount = 1;
+/* ----------------------------------------------------------在没进入框架前的处理--------------------------------------------------------------- */
+
+/* 初次更新进度 */
+void upDateProgress(OLEDDisplay *display) {  //首次天气更新
+String tag = "normal";
+  drawProgress(display, 0, "open spiffs");
+  if (SPIFFS.begin()) { // 打开闪存文件系统 
+    Serial.println("闪存文件系统打开成功");
+  } else {
+     tag = "error";
+     Serial.println("闪存文件系统打开失败");
+  }
+  if(SPIFFS.exists("/config.json")){ // 判断有没有config.json这个文件
+    Serial.println("存在配置信息，正在自动连接");
+    const size_t capacity = JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(2) + 60; //分配一个内存空间
+    DynamicJsonDocument doc(capacity);// 声明json处理对象
+    File configJson = SPIFFS.open("/config.json", "r");
+    deserializeJson(doc, configJson); // json数据序列化
+    const char* ssid = doc["ssid"];
+    const char* password = doc["password"];
+    WiFi.mode(WIFI_STA); // 更换wifi模式
+    WiFi.begin(ssid, password); // 连接wifi
+    while(WiFi.status()!= WL_CONNECTED){
+      delay(500);
+      Serial.println(".");  
+    }
+    configJson.close();
+    Serial.println(WiFi.localIP());
+    /* 页面框架定义 */
+    FrameCallback frames[] = { drawDateTime, drawWeather, drawFrame3, drawFrame4, drawFrame5 };
+    int frameCount = 5;
+
+    OverlayCallback overlays[] = { drawIpOverlay };
+    int overlaysCount = 1;
+  } else {
+    Serial.println("不存在配置信息，正在打开web配网模式"); 
+    IPAddress softLocal(192,168,1,1);
+    IPAddress softGateway(192,168,1,1);
+    IPAddress softSubnet(255,255,255,0);
+    WiFi.softAPConfig(softLocal, softGateway, softSubnet);
+    WiFi.softAP("esp8266", "12345678"); //这里是配网模式下热点的名字和密码
+    Serial.println(WiFi.softAPIP());
+    FrameCallback frames[] = { drawDateTime, drawWeather, drawFrame3, drawFrame4, drawFrame5 };
+    int frameCount = 5;
+    OverlayCallback overlays[] = { drawIpOverlay };
+    int overlaysCount = 1;
+  }
+  server.on("/", handleRoot);//web首页监听
+  server.on("/set", handleConnect); // 配置ssid密码监听，感觉跟express的路由好像
+  server.begin();
+  delay(500);
+  drawProgress(display, 20, tag);
+  delay(1000);
+  drawProgress(display, 50, "Please wait Updating forecasts...");
+  configTime(TZ_SEC, DST_SEC, "ntp.ntsc.ac.cn", "ntp1.aliyun.com"); //ntp获取时间同步系统时间戳
+  drawProgress(display, 80, "Updating...");
+   weatherNow.config(UserKey, Location, Unit, Lang);
+  if(weatherNow.get()){ // 获取天气更新
+    int icon_code = weatherNow.getIcon();
+    WEATHER_ICON = getIcons(String(icon_code));
+    TEMP =  String(weatherNow.getTemp());
+    String type = getWeatherType(icon_code);
+    HUM = weatherNow.getHumidity();
+    length = type.length() / 3;
+    for(int i =0 ; i<type.length(); i++ ) {
+      WEATHER_TYPE[i] = type[i];
+    }
+  }
+  drawProgress(display, 100, "Wellcome...");
+  delay(200);
+}
 
 
 /* ------------------------------------------------------------方法和数据更新----------------------------------------------------------------- */
+
+/* 单个进度条方法 */
+void drawProgress(OLEDDisplay *display, int progress, String label) {
+  display->clear();
+  display->setTextAlignment(TEXT_ALIGN_CENTER);
+  display->setFont(ArialMT_Plain_10);
+  display->drawString(64, 10, label);
+  display->drawProgressBar(2, 28, 124, 10, progress);
+  display->display();
+}
 
 /* IP地址转字符串 */
 String ip2Str(IPAddress ip) { 
@@ -264,25 +308,64 @@ void drawChinese(OLEDDisplay *display, int x, int y, const char chinese[]) {
   }
 }
 
-/* WiFi配网 */
-void wificonnect() {  
-  WiFi.begin(WIFI_SSID, WIFI_PWD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(80);
+void handleRoot() { //展示网页的关键代码
+  Serial.println("访问");
+  Dir dir = SPIFFS.openDir("/");
+  while (dir.next()){
+    Serial.println(dir.fileName());
   }
-  delay(500);
+  if(SPIFFS.exists("/index.html")){
+      File index = SPIFFS.open("/index.html", "r");
+      server.streamFile(index, "text/html");
+      index.close();
+  }  
+}
+
+void handleConnect() { //处理配置信息的函数
+  String ssid = server.arg("ssid");   //arg是获取请求参数，视频中最后面展示了请求的完整链接
+  String password = server.arg("password"); 
+  server.send(200, "text/plain", "OK");
+  WiFi.mode(WIFI_STA); //改变wifi模式
+  WiFi.begin(ssid.c_str(), password.c_str());//String类型直接用会报错，不要问为什么，转成char *就行了。
+  while(WiFi.status()!= WL_CONNECTED){
+     delay(500);
+     Serial.println(".");  
+  }
+  Serial.println(WiFi.localIP());
+  removeConfig(); // 不管有没有配置先删除一次再说。
+  String payload; // 拼接构造一段字符串形式的json数据长{"ssid":"xxxxx","password":"xxxxxxxxxxx"}
+  payload += "{\"ssid\":\"";
+  payload += ssid;
+  payload +="\",\"password\":\"";
+  payload += password;
+  payload += "\"}";
+  File wifiConfig = SPIFFS.open("/config.json", "w");
+  wifiConfig.println(payload);//将数据写入config.json文件中
+  wifiConfig.close();
+}
+
+void removeConfig(){
+    if(SPIFFS.exists("/config.json")){ // 判断有没有config.json这个文件
+      if (SPIFFS.remove("/config.json")){
+         Serial.println("删除旧配置");
+      } else {
+        Serial.println("删除旧配置失败");
+      } 
+    }
 }
 
 
 /* ----------------------------------------------------------主进程--------------------------------------------------------------- */
 void setup() {
+  Serial.begin(115200);   
+  Serial.println("");
   display.init();
   display.clear();
   display.display();
   display.flipScreenVertically();
   upDateProgress(&display);
-  pinMode(3, INPUT);
-  pinMode(1, INPUT);
+  // pinMode(3, INPUT_PULLUP);
+  // pinMode(1, INPUT_PULLUP);
   tick_key.attach_ms(1, keyScan);
   IP_ADDRESS = ip2Str(WiFi.localIP());
   ui.setTargetFPS(45);
@@ -303,8 +386,11 @@ void setup() {
 /* ----------------------------------------------------------循环（定时更新数据）--------------------------------------------------------------- */
 void loop() {
   display.clear();
+  server.handleClient();
+
   int remainingTimeBudget = ui.update();
   if (remainingTimeBudget > 0) {
+    
     delay(remainingTimeBudget);
   }
 }
